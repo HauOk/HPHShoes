3e65d207177cf1742a99c2cb181690e707a7053f
Hau <akirahaku2@gmail.com>
2022-06-28 21:08:47 +0700

    upload code

---
+++ OnlineShop/OnlineShop.API/Controllers/NikeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.API.Controllers
+{
+    public class NikeController : Controller
+    {
+        // GET: Nike
+        OnlineShopDbContext data = new OnlineShopDbContext();
+        public ActionResult Index(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 12;
+
+            return View(data.Products.Where(x => x.CategoryID == 1).ToList().OrderBy(n => n.ID).ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
